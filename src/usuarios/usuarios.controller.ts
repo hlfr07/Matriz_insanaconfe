80a5a7ad3535957bf96fe2f3fca718b1ec4522fb
hlfr07 <hf46.78@gmail.com>
2024-10-24 10:28:49 -0500

    HABILITANDO LA CONSULTA PARA SUS PERMISOS DE CADA USUARIOS

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -10,46 +10,51 @@ import { UpdatePasswordCodeUsuarioDto } from './dto/updatepasswordcode-usuarios.
 @ApiTags('Usuarios')
 @Controller('usuarios')
 export class UsuariosController {
-  constructor(private readonly usuariosService: UsuariosService) {}
+  constructor(private readonly usuariosService: UsuariosService) { }
 
-  @ApiBody({type: CreateUsuarioDto})
+  @ApiBody({ type: CreateUsuarioDto })
   @Post()
   create(@Body() createUsuarioDto: CreateUsuarioDto) {
     return this.usuariosService.create(createUsuarioDto);
   }
 
-  @ApiBody({type: [GetUsuarioDto]})
+  @ApiBody({ type: [GetUsuarioDto] })
   @Get()
   findAll() {
     return this.usuariosService.findAll();
   }
 
-  @ApiBody({type: [GetUsuarioDto]})
+  @ApiBody({ type: [GetUsuarioDto] })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usuariosService.findOne(+id);
   }
 
-  @ApiBody({type: UpdateUsuarioDto})
+  @ApiBody({ type: UpdateUsuarioDto })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
     return this.usuariosService.update(+id, updateUsuarioDto);
   }
 
-  @ApiBody({type: UpdatePasswordUsuarioDto})
+  @ApiBody({ type: UpdatePasswordUsuarioDto })
   @Post('/solicitarresetpassword')
   updatePassword(@Body() updatePasswordUsuarioDto: UpdatePasswordUsuarioDto) {
     return this.usuariosService.updatePassword(updatePasswordUsuarioDto);
   }
 
-  @ApiBody({type: UpdatePasswordCodeUsuarioDto})
+  @ApiBody({ type: UpdatePasswordCodeUsuarioDto })
   @Patch('/actualizarpassword/:dni')
-  updatePasswordCode(@Param('dni') dni: string,@Body() updatePasswordCodeUsuarioDto: UpdatePasswordCodeUsuarioDto) {
+  updatePasswordCode(@Param('dni') dni: string, @Body() updatePasswordCodeUsuarioDto: UpdatePasswordCodeUsuarioDto) {
     return this.usuariosService.updatePasswordCode(dni, updatePasswordCodeUsuarioDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usuariosService.remove(+id);
+  }
+
+  @Get('/permisosporidusuario/:id')
+  buscarpermisosporidusuario(@Param('id') id: string) {
+    return this.usuariosService.buscarpermisosporidusuario(+id);
   }
 }
